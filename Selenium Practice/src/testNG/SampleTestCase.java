040a9a0172a3572bdc1e4dc85340928e26839e54
HP <HP@DESKTOP-LDK6U86>
2023-03-11 16:10:29 +0530

    Listeners in TestNG

--- Selenium Practice/src/testNG/SampleTestCase.java
+++ Selenium Practice/src/testNG/SampleTestCase.java
@@ -8,17 +8,15 @@ public class SampleTestCase {
 	public void firstTestCase() {
 		System.out.println("This is the first test case");  //method 1
 	}
-	@Test
-	public void secondTestCase() {
-		System.out.println("This is the second test case");  //method 2
-	}
-	@Test
-	public void thirdTestCase() {
-		System.out.println("This is the third test case");   //method 3
-	}
-	@Test
-	public void forthTestCase() {
-		System.out.println("This is the fourth test case");   //method4
-	}
+	/*
+	 * @Test public void secondTestCase() {
+	 * System.out.println("This is the second test case"); //method 2 }
+	 * 
+	 * @Test public void thirdTestCase() {
+	 * System.out.println("This is the third test case"); //method 3 }
+	 * 
+	 * @Test public void forthTestCase() {
+	 * System.out.println("This is the fourth test case"); //method4 }
+	 */
 
 }
